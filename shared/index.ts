e2fcdf5695c9cfa54db139604cf916256b01f766
fredhel KISSIE <fredkiss3@gmail.com>
2022-05-21 03:42:08 +0200

    🏷️ Add a shared folder for socket event types

---
+++ shared/index.ts
@@ -0,0 +1,56 @@
+export enum SocketClientEvent {
+    OfferRequested = 'client:offer-requested',
+    AnswerRequested = 'client:answer-requested',
+    OfferSent = 'client:offer-sent',
+    AnswerSent = 'client:answer-sent',
+    Disconnected = 'client:disconnected',
+    RoomCreated = 'client:room-created',
+}
+
+export interface ClientEventMap {
+    [SocketClientEvent.OfferRequested]: (arg: { peerId: string }) => void;
+    [SocketClientEvent.AnswerRequested]: (arg: {
+        peerId: string;
+        sdpOffer: object;
+        iceCandidates: object[];
+    }) => void;
+    [SocketClientEvent.OfferSent]: (arg: {
+        peerId: string;
+        sdpOffer: object;
+        candidates: object[];
+    }) => void;
+    [SocketClientEvent.AnswerSent]: (arg: {
+        peerId: string;
+        sdpAnswer: object;
+        candidates: object[];
+    }) => void;
+    [SocketClientEvent.Disconnected]: (disconnectedPeerIds: string[]) => void;
+    [SocketClientEvent.RoomCreated]: (arg: { roomId: string }) => void;
+}
+
+export enum SocketServerEvent {
+    CreateRoom = 'server:create-room',
+    JoinRoom = 'server:join-room',
+    SendOffer = 'server:send-offer',
+    SendAnswer = 'server:send-answer',
+    Disconnected = 'server:disconnected',
+}
+
+export interface ServerEventMap {
+    [SocketServerEvent.CreateRoom]: (roomName: string) => void;
+    [SocketServerEvent.JoinRoom]: (arg: {
+        roomId: string;
+        clientName: string;
+    }) => void;
+    [SocketServerEvent.SendOffer]: (arg: {
+        peerId: string;
+        sdpOffer: object;
+        candidates: object[];
+    }) => void;
+    [SocketServerEvent.SendAnswer]: (arg: {
+        peerId: string;
+        sdpAnswer: object;
+        candidates: object[];
+    }) => void;
+    [SocketServerEvent.Disconnected]: (arg: { roomId: string }) => void;
+}
