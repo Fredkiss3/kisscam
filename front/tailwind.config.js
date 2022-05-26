9d07837d7076c405379a7741982d91d147488dbe
fredhel KISSIE <fredkiss3@gmail.com>
2022-05-26 22:38:01 +0200

    🔧  Added tailwind configuration

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#00C966',
+        danger: '#ff005a',
+        secondary: '#928c9e',
+        hollow: '#261b3c',
+        darkest: '#020016',
+        darker: '#0d0a16',
+        dark: '#100a1f'
+      }
+    },
   },
   plugins: [],
 }
