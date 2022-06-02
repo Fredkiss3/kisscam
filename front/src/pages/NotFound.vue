b2fc3a4d0eb9204c51715cf602e24167d98b26ed
fredhel KISSIE <fredkiss3@gmail.com>
2022-06-02 22:44:55 +0200

    💄 Update the NotFound Component to accept an optional message

--- front/src/pages/NotFound.vue
+++ front/src/pages/NotFound.vue
@@ -1,5 +1,22 @@
 <template>
-    <div class="h-screen flex items-center justify-center">
-        404 Not Found
+    <div class="h-screen flex items-center justify-center flex-col gap-4">
+        <h1>{{ props.message }}</h1>
+
+        <a href="#/" class="flex gap-2 items-center underline">
+            <ArrowLeftIcon class="h-4" />
+            Go Home
+        </a>
     </div>
 </template>
+
+<script setup lang="ts">
+import { ArrowLeftIcon } from '@heroicons/vue/outline';
+
+interface Props {
+    message?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    message: '404 - Page not found'
+});
+</script>
