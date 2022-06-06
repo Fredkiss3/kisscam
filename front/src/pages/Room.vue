3fe33afba6475a93036ea20ec504693fce6f08a1
fredhel KISSIE <fredkiss3@gmail.com>
2022-06-07 00:15:49 +0200

    💄  Show all connected users and show current user's video

--- front/src/pages/Room.vue
+++ front/src/pages/Room.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        v-if="currentStep === 'JOINING_ROOM'"
+        v-if="store.currentStep === 'JOINING_ROOM'"
         class="flex gap-2 items-center justify-center h-screen w-full"
     >
         <Loader />
@@ -9,70 +9,44 @@
     </div>
 
     <div
-        v-else-if="currentStep === 'ROOM_JOINED'"
+        v-else-if="store.currentStep === 'ROOM_JOINED'"
         class="h-screen flex flex-col items-center justify-center gap-4 p-4 max-w-[1200px] m-auto"
     >
-        <h1 class="font-bold text-4xl">You are in room {{ name }}</h1>
+        <h1 class="font-bold text-4xl">
+            You are in room {{ store.room.name }}
+        </h1>
 
-        <div class="flex gap-6 flex-col md:flex-row w-full">
-            <!-- <VideoCard
-                talking
-                :pinned="currentPinnedId === 'randeowdwehf;lwrih'"
-                audioActive
-                name="Alan Becker"
-                :peepsNo="peepsNo.second"
-                peerId="randeowdwehf;lwrih"
-                @pin="(peerId) => (currentPinnedId = peerId)"
-                class="!w-full md:!h-[500px] max-w-[800px]"
+        <div class="flex flex-wrap gap-6 justify-center">
+            <VideoCard
+                name="You"
+                :video-src="store.user.stream"
+                :client-id="store.user.id!"
+                :peeps-no="randomInt(1, 105)"
             />
 
-            <div class="flex flex-wrap gap-6 md:flex-col justify-center">
-                <VideoCard
-                    name="You"
-                    minimized
-                    :pinned="currentPinnedId === 'randeowdwehf;lwrihr'"
-                    :peepsNo="peepsNo.first"
-                    peerId="randeowdwehf;lwrihr"
-                    @pin="(peerId) => (currentPinnedId = peerId)"
-                    class="h-40 w-[225px]"
-                />
-
-                <VideoCard
-                    name="You"
-                    minimized
-                    :pinned="currentPinnedId === 'randeowdwehf;lwrihrded'"
-                    :peepsNo="peepsNo.first"
-                    peerId="randeowdwehf;lwrihrded"
-                    @pin="(peerId) => (currentPinnedId = peerId)"
-                    class="h-40 w-[225px]"
-                />
-            </div> -->
+            <VideoCard
+                v-for="client in store.room.clients"
+                :key="client.clientId"
+                :name="client.clientName"
+                :client-id="client.clientId"
+                :peeps-no="client.peepNo"
+            />
         </div>
-
-        <!-- <Button @click="randomizePeeps">
-            <span>Radomize Peeps</span> <RefreshIcon class="h-4" />
-        </Button> -->
     </div>
 
     <NotFound v-else message="Room not found" />
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, onUnmounted } from 'vue';
 import NotFound from '../pages/NotFound.vue';
-// import Button from '../components/Button.vue';
-// import { randomInt } from '../lib/functions';
-// import { RefreshIcon } from '@heroicons/vue/outline';
+import { randomInt } from '../lib/functions';
 import { useStore } from '../lib/store';
 import Loader from '../components/Loader.vue';
 import { gotoHashURL } from '../lib/functions';
+import VideoCard from '../components/VideoCard.vue';
 
-const {
-    user,
-    joinRoom,
-    room: { id, name },
-    currentStep
-} = useStore();
+const store = useStore();
 
 const hashFromID = computed(() => {
     const roomRegex = /\/room\/([a-z0-9]{10})$/;
@@ -80,9 +54,8 @@ const hashFromID = computed(() => {
     return hash.match(roomRegex)![1];
 });
 
-onMounted(() => {
-    if (!user.name) {
-        // window.location.hash = `/join-room`;
+onMounted(async () => {
+    if (!store.user.name) {
         gotoHashURL('/join-room', {
             'room-id': hashFromID.value
         });
@@ -90,6 +63,22 @@ onMounted(() => {
         return;
     }
 
-    joinRoom(hashFromID.value, user.name);
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+    });
+
+    if (stream) {
+        store.user.stream = stream;
+    }
+
+    store.joinRoom({
+        id: hashFromID.value,
+        username: store.user.name
+    });
+});
+
+onUnmounted(() => {
+    store.leaveRoom();
 });
 </script>
