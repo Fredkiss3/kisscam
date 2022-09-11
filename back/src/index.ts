bc16bb435f3a65db5f9d97dfd65654b8a4eaba4f
fredhel KISSIE <fredkiss3@gmail.com>
2022-09-11 20:53:06 +0200

    🐛 use port 8080 by default

--- back/src/index.ts
+++ back/src/index.ts
@@ -66,7 +66,7 @@ server.ready().then(() => {
 // Start the server
 const start = async () => {
     server.listen(
-        { port: Number(process.env.PORT), host: `0.0.0.0` },
+        { port: Number(process.env.PORT ?? 8080), host: `0.0.0.0` },
         (err, address) => {
             if (err) {
                 console.error(err);
