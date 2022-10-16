d402cb192dade1ee16c1442214c02069d0ba8113
Adrien KISSIE <fredkiss3@gmail.com>
2022-10-16 21:00:16 +0200

    📈 Add analytics code

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,3 +1,17 @@
 <template>
     <router-view />
+    <component
+        is="script"
+        v-if="isPROD"
+        async
+        defer
+        data-website-id="b609746d-2ca7-4be3-be87-cb1555ed2205"
+        src="https://monitor.fredkiss.dev/umami.js"
+    ></component>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isPROD = ref(import.meta.env.MODE !== 'development');
+</script>
