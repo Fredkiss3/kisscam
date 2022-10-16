f0c004811a0063301aa9f8446e0b04c4f92445a2
Adrien KISSIE <fredkiss3@gmail.com>
2022-10-16 21:05:43 +0200

    📈 Use the correct id for analytics

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,7 +5,7 @@
         v-if="isPROD"
         async
         defer
-        data-website-id="b609746d-2ca7-4be3-be87-cb1555ed2205"
+        data-website-id="fc44ac46-b799-4d8c-b9a3-3cb36821b134"
         src="https://monitor.fredkiss.dev/umami.js"
     ></component>
 </template>
@@ -13,5 +13,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
+// @ts-ignore
 const isPROD = ref(import.meta.env.MODE !== 'development');
 </script>
