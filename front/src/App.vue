b645c2bb6639ffe677298622aa76ce589313863f
Adrien KISSIE <fredkiss3@gmail.com>
2022-10-16 21:11:46 +0200

    💚 Fix this shit

--- front/src/App.vue
+++ front/src/App.vue
@@ -11,8 +11,6 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-
 // @ts-ignore
-const isPROD = ref(import.meta.env.MODE !== 'development');
+const isPROD = import.meta.env.MODE !== 'development';
 </script>
