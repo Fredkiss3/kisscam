9df224cded7d0fb7e01377b16dc8a0d6de1b3108
Adrien KISSIE <fredkiss3@gmail.com>
2022-10-19 23:12:34 +0200

    💄 Add copyright note in footer

--- front/src/pages/Layout.vue
+++ front/src/pages/Layout.vue
@@ -2,4 +2,19 @@
     <div class="h-screen flex flex-col items-center justify-center gap-4">
         <router-view></router-view>
     </div>
+
+    <footer
+        class="fixed bottom-0 left-0 right-0 p-2 bg-dark flex items-center justify-center"
+    >
+        <div class="text-center">
+            Copyright &copy; 2022 By
+            <a target="_blank" href="https://fredkiss.dev" class="underline">
+                Adrien KISSIE</a
+            >
+            Contact me at
+            <a class="font-bold underline" href="mailto:contact@kiss-cam.live"
+                >contact@kiss-cam.live</a
+            >
+        </div>
+    </footer>
 </template>
