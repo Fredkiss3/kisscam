b4458b37a96d777993300ecb83380de6349f2e83
Adrien KISSIE <fredkiss3@gmail.com>
2022-11-27 08:01:40 +0100

    Add cloudfare analytics

--- front/src/App.vue
+++ front/src/App.vue
@@ -8,6 +8,14 @@
         data-website-id="fc44ac46-b799-4d8c-b9a3-3cb36821b134"
         src="https://monitor.fredkiss.dev/umami.js"
     ></component>
+    <component
+        is="script"
+        v-if="isPROD"
+        async
+        defer
+        src='https://static.cloudflareinsights.com/beacon.min.js' 
+        data-cf-beacon='{"token": "8a495778bcbd488ab65719ae13d706e9"}'
+    ></component>
 </template>
 
 <script setup lang="ts">
